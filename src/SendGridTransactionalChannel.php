72304bdc3228996c378c8bb1592e21a7c15c9c38
Juan <litvinjuan@gmail.com>
2020-03-09 17:01:25 -0300

    Fix indentation

--- src/SendGridTransactionalChannel.php
+++ src/SendGridTransactionalChannel.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace LaravelSendGridChannel;
-use GuzzleHttp\Client;
 
+use GuzzleHttp\Client;
 use Illuminate\Notifications\Notification;
 
 class SendGridTransactionalChannel
@@ -27,7 +27,7 @@ class SendGridTransactionalChannel
                     'personalizations' => [
                         [
                             'to' => $data['to'],
-			    'dynamic_template_data' => $data['data'],
+                            'dynamic_template_data' => $data['data'],
                         ],
                     ],
                     'template_id' => $data['template_id']
