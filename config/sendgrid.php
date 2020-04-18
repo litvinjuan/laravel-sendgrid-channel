eb1e8fc97cf052206dec3920dc2468bbecab3b05
Juan <litvinjuan@gmail.com>
2020-04-18 14:21:12 -0300

    Fix dependences

--- config/sendgrid.php
+++ config/sendgrid.php
@@ -3,7 +3,6 @@
 return [
 
     'api-key' => env('SEND_GRID_API_KEY'),
-
-    'sandbox-enabled' => env('SEND_GRID_SANDBOX_ENABLED', true),
+    'sandbox-enabled' => env('SEND_GRID_SANDBOX_ENABLED', false),
 
 ];
